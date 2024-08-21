d9e08a2de5776c443e31b516164b34026c11f95d
José Carlos Delgado Ramos <jcdelgadoramos@outlook.com>
2024-08-21 16:14:56 -0500

    refs #2 Animals and Adoptions model structure

--- animalrescue/adoptions/models.py
+++ animalrescue/adoptions/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
-# Create your models here.
+from animals.models import Animal
+from people.models import User
+
+
+class AdoptionState(models.Model):
+    """
+    Class for AdoptionState
+    Sets the state for the adoption
+    """
+
+    state = models.CharField("State", max_length=255)
+    creation_date = models.DateTimeField("Creation date", auto_now_add=True)
+    modification_date = models.DateTimeField("Modification date", auto_now=True)
+
+    class Meta:
+        verbose_name = "Adoption state"
+        verbose_name_plural = "Adoption states"
+
+
+class Adoption(models.Model):
+    """
+    Class for Adoption
+    Stores information for an adoption of an Animal by an Adoptant
+    """
+
+    animal = models.OneToOneField(
+        Animal,
+        verbose_name="Animal",
+        on_delete=models.CASCADE,
+    )
+    adoptant = models.ForeignKey(
+        User,
+        verbose_name="Adoptant",
+        on_delete=models.CASCADE,
+        related_name="adoptions",
+    )
+    volunteer = models.ForeignKey(
+        User,
+        verbose_name="Volunteer",
+        on_delete=models.CASCADE,
+        related_name="adoptions_registered",
+    )
+    adoption_date = models.DateTimeField("Adoption date", auto_now_add=True)
+    state = models.ForeignKey(
+        AdoptionState,
+        verbose_name="State",
+        on_delete=models.CASCADE,
+        related_name="adoptions",
+    )
--- animalrescue/animals/models.py
+++ animalrescue/animals/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
-# Create your models here.
+
+class AnimalType(models.Model):
+    """
+    Class for AnimalType model.
+    An AnimalType indicates the species of the animal.
+    """
+
+    name = models.CharField("Animal type", max_length=255)
+    scientific_name = models.CharField("Scientific name", max_length=255)
+    creation_date = models.DateTimeField("Creation date", auto_now_add=True)
+    modification_date = models.DateTimeField("Modification date", auto_now=True)
+
+    class Meta:
+        verbose_name = "Animal type"
+        verbose_name_plural = "Animal types"
+
+
+class AdoptionStatus(models.Model):
+    """
+    Class for AdoptionStatus model.
+    Indicates the adoption status of an Animal
+    """
+
+    status = models.CharField("Status", max_length=255)
+    creation_date = models.DateTimeField("Creation date", auto_now_add=True)
+    modification_date = models.DateTimeField("Modification date", auto_now=True)
+
+    class Meta:
+        verbose_name = "Adoption status"
+        verbose_name_plural = "Adoption statuses"
+
+
+class Animal(models.Model):
+    """
+    Class for Animal model.
+    Is related to every single animal stored.
+    """
+
+    name = models.CharField("Name", max_length=255)
+    breed = models.CharField("Breed", max_length=255)
+    age = models.IntegerField("Age", default=0)
+    animal_type = models.ForeignKey(
+        AnimalType,
+        verbose_name="Animal type",
+        on_delete=models.CASCADE,
+        related_name="animals",
+    )
+    adoption_status = models.ForeignKey(
+        AdoptionStatus,
+        verbose_name="Adoption status",
+        on_delete=models.CASCADE,
+        related_name="animals",
+    )
+
+    class Meta:
+        verbose_name = "Animal"
+        verbose_name_plural = "Animals"
