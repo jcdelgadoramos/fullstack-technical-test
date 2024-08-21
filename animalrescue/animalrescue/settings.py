e1a5b02cc4ea53b900c7780a24130e3aa38153b4
José Carlos Delgado Ramos <jcdelgadoramos@outlook.com>
2024-08-21 15:20:47 -0500

    ref #1 User + user group setup

--- animalrescue/animalrescue/settings.py
+++ animalrescue/animalrescue/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "rest-framework",
+    "rest_framework",
 ]
 
 LOCAL_APPS = [
